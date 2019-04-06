8c2c251f3c927176ef84f6e3ca30bd941d446e00
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-04-06 09:36:31 +0200

    Update CalculatorBot.py

--- CalculatorBot.py
+++ CalculatorBot.py
@@ -15,13 +15,39 @@ async def on_ready():
     await client.change_presence(game=Game(name='Calculating by + | - | x | ÷ || c'))
     print('Connected on ' + client.user.name)
 
+@client.command()
+async def add(left: int, right: int):
+    await client.say(left + right)
+
+@client.command()
+async def subtract(left: int, right: int):
+    await client.say(left - right)
+
+@client.command()
+async def multiply(left: int, right: int):
+    await client.say(left * right)
+
+@client.command()
+async def divide(left: int, right: int):
+    try:
+        await client.say(left // right)
+    except ZeroDivisionError:
+        await client.say("Can't be divided by ZERO")
+
 @client.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
     channel = ctx.message.author
 
     embed = discord.Embed(
-        colour=discord.Colour.green()
+        colour=discord.Colour.brown()
     )
     embed.set_author(name='Help')
-    embed.add_field(name='add', value='Adds a number to a number')
+    embed.add_field(name='add', value='Adds a number to a number', inline=False)
+    embed.add_field(name='subtract', value='Subtracts a number from another number', inline=False)
+    embed.add_field(name='multiply', value='Multiplys a number', inline=False)
+    embed.add_field(name='divide', value='Divides a number', inline=False)
+
+    await client.send_message(channel, embed=embed)
+
+client.run(str(os.environ.get('BOT_TOKEN')))
